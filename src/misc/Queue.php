35a10c56423ce5f2ba9bddc0b47b483f270c80ab
Bruno Natali <b010010010n@gmail.com>
2019-10-02 14:44:11 -0300

    Implemented listing, for waiting answers, and real queue

--- src/misc/Queue.php
+++ src/misc/Queue.php
@@ -1,23 +1,130 @@
+<?php declare(strict_types=1);
+
+/**                                LICENSE
+ *     The MIT License (MIT)
+ *
+ *     Copyright (c) 2019  Bruno Natali
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of
+ *  this software and associated documentation files (the "Software"), to deal in
+ *  the Software without restriction, including without limitation the rights to
+ *  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ *  the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ *  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ *  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ *  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ *  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace BrunoNatali\SysTema\Misc;
 
 use SplQueue;
+use BrunoNatali\EventLoop\LoopInterface;
 
-Private $queue;
-        $this->queue = new SplDoublyLinkedList;
+class Queue
+{
+    Private $main;
+    Private $loop;
+    Private $timer;
+    Private $listById = [];
+    Private $running = false;
+    Private $enabled = false;
+    Private $waitToRun = false;
+    Private $lastErrorString;
 
- Public function performData($data = null)
- {
-     if ($data !== null) {
-         $this->queue->push();
-     } else {
-         try {
-             if (call_user_func([$this, 'encode'], $stored = $this->queue->pop()) === 0)
-                 return $stored['data'];
+    function __construct(LoopInterface &$loop)
+    {
+        $this->main = new SplQueue();
+        $this->loop = &$loop;
+    }
 
-             return null;
-         } catch (RuntimeException $e) {
-             $this->lastErrorString = $e;
-             return null;
-         }
-     }
+    Public function listAdd(string $id = "", $onAdd = null, $onData = null)
+    {
+        if ($id != "") $this->listById[$id] = [
+            'id' => $id,
+            'onAdd' => $onAdd,
+            'onData' => $onData
+        ];
+        if (is_callable($this->listById[$id]['onAdd'])) {
+            echo "is callable";
+            return $this->listById[$id]['onAdd']($params = $this->listById[$id]);
+        }
+        return true;
+    }
 
- }
+    Public function listProccess(string $id, $data = null)
+    {
+        $toReturn = true;
+        if (!isset($this->listById[$id])) return false;
+
+        if (is_callable($this->listById[$id]['onData'])) {
+            $toReturn = $this->listById[$id]['onData']($data, $params = $this->listById[$id]);
+        }
+
+        $this->listRemove($id);
+
+        return $toReturn;
+    }
+
+    Public function listRemove(string $id)
+    {
+        if (isset($this->listById[$id])) unset($this->listById[$id]);
+    }
+
+    Public function push(mixed $value, number $id = null)
+    {
+        if ($id !== null) {
+            try {
+                $this->main->add($id,$value);
+            } catch (OutOfRangeException $e) {
+                $this->lastErrorString = $e;
+                return false;
+            }
+        } else {
+            $this->main->enqueue($value);
+        }
+
+        return $this->next();
+    }
+
+    Public function next()
+    {
+        if ($this->main->isEmpty()) return null;
+        if (!$this->enabled || $this->running) {
+            $this->waitToRun = true;
+            return false;
+        }
+
+        $queueExecuteNode = $this->main->dequeue();
+        if (is_callable()) {
+            $this->running = true;
+            $this->timer = $this->loop->addTimer(0.1, function () use ($queueExecuteNode){
+                $queueExecuteNode();
+                $this->running = false;
+                $this->timer = null;
+            });
+        }
+        return true;
+    }
+
+    Public function pause()
+    {
+        $this->enabled = false;
+    }
+
+    Public function resume()
+    {
+        $this->enabled = true;
+        if ($this->waitToRun) {
+            $this->waitToRun = false;
+            $this->next();
+        }
+    }
+}
